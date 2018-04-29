ea441938167fff1e5a026b193a7761cc5ce67d65
M.E. Hoagland <mehoagland@MEs-MacBook-Pro.local>
2018-04-29 18:16:06 -0400

    got on click working on drink name, in react console returning drink info

--- thirsty/client-app/src/components/DrinkList.js
+++ thirsty/client-app/src/components/DrinkList.js
@@ -1,15 +1,42 @@
 import React, { Component } from "react";
 import api from "../api";
+import DrinkSingle from './DrinkSingle';
 
 class DrinkList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       apiDataLoaded: false,
-      drinkData: null
+      drinkData: null,
+      singleDrinkData: null,
+      singleDrinkDataLoaded: false
     };
     this.renderDrinks = this.renderDrinks.bind(this);
   }
+
+  displayInfo(strDrink) {
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${strDrink}`)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+        this.setState({
+          singleDrinkData: response.drinks,
+          singleDrinkDataLoaded: true,
+        });
+      })
+      .catch(err => console.log(err));
+    }
+
+    //   render() {
+    // return (
+    //   <div>
+    //     {this.props.allPokemon.map(item => {
+    //       return (
+    //         <div>
+    //           <h1 onClick={() => this.props.displayInfo(item.url) }>{item.name}</h1>
+    //         </div>
+    //       );
+    //     })}
 
 
   componentDidMount() {
@@ -27,8 +54,8 @@ class DrinkList extends Component {
   renderDrinks() {
     return this.state.drinkData.map( drink =>
 
-        <div className="all-drinks">
-          <div className='image'><img src={drink.strDrinkThumb} className='main-drinks'/></div> <div className='drink-name'>{drink.strDrink}</div>
+        <div className="all-drinks" >
+          <div className='image'><img src={drink.strDrinkThumb} className='main-drinks'/></div> <div className='drink-name' onClick={() => this.displayInfo(drink.strDrink)}>{drink.strDrink}</div>
         </div>
 
 
@@ -38,13 +65,18 @@ class DrinkList extends Component {
   render() {
     return (
       <div className="wrapper">
-      <div className = 'container'>
-      {this.state.apiDataLoaded ? ( this.renderDrinks()
+        <div className = 'container'>
+          {this.state.apiDataLoaded ? ( this.renderDrinks()
+
     ) : (
-      <div>Loading</div>
+        <div>Loading</div>
     )}
+    <DrinkSingle drinkSingle={this.state.singleDrinkData}
+                  singleDataLoaded={this.state.singleDrinkDataLoaded}/>
     </div>
     </div>
+
+
   )}
 }
 
---
+++ thirsty/client-app/src/components/DrinkSingle.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+class DrinkSingle extends Component {
+
+  constructor(props) {
+
+    super(props);
+
+    this.state = {};
+  }
+
+
+
+  render () {
+
+    return (
+      <div>Hello></div>
+    );
+  }
+}
+
+export default DrinkSingle;
